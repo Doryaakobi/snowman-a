e5651bc0fae16e6ae3f0f5693e6bbe1339252a6f
Dor <doryaakobi@gmail.com>
2021-03-10 18:27:15 +0200

    Initial

---
+++ snowman.cpp
@@ -0,0 +1,23 @@
+#include "snowman.hpp"
+using namespace std;
+#define LLENGHT  8
+#define MaxVal  4
+#define MinVal  1
+
+namespace ariel{
+
+    string snowman(int input){
+
+
+        string str = "so you want to build a snowman";
+       
+        return str;
+    }
+}
+
+
+
+
+
+
+
